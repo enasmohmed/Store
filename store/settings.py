7ad5f9608f2376639f5ca9dfe9403528df86069d
EnasMohamed <49351823+enasmohmed@users.noreply.github.com>
2020-11-24 12:25:40 +0200

    Update settings.py

--- store/settings.py
+++ store/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
