24fe185ad8ebd7e948c84f1c03e568b356efaeb1
Enas <enasm2477@gmail.com>
2020-11-24 13:13:15 +0200

    add config

--- store/settings.py
+++ store/settings.py
@@ -126,18 +126,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-<<<<<<< HEAD
-=======
 
->>>>>>> main
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-<<<<<<< HEAD
-=======
 
->>>>>>> main
+
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
