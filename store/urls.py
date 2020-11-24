da69d39a67879abab4d61ce8344b83b6b204a273
Enas <enasm2477@gmail.com>
2020-11-24 13:32:15 +0200

    urls

--- store/urls.py
+++ store/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.shortcuts import redirect
 from django.urls import path, include
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 
 def home(request):
