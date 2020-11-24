7a5c652cdbfa5843e77de0344dafc5706b8ccf99
Enas <enasm2477@gmail.com>
2020-11-24 22:50:50 +0200

    Add email settings

--- store/prod_settings.py
+++ store/prod_settings.py
@@ -5,7 +5,7 @@ import dj_database_url
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = False
+DEBUG = Tr
 
 ALLOWED_HOSTS = ['djangonewapp.herokuapp.com']
 
@@ -15,3 +15,15 @@ DATABASES_URL = os.environ.get('DATABASES_URL')
 DATABASES = {
     'default': dj_database_url.config(default=os.environ.get('DATABASES_URL'))
 }
+
+# Email
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
+
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey' # this is exactly the value 'apikey'
+EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
